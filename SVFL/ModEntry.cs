4e68ae311ef3731afd584267f2ae4d97b3b2f2d2
Vox <pgsvox@gmail.com>
2020-02-22 09:57:05 -0600

    Add project files.

---
+++ SVFL/ModEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+
+namespace SVFL
+{
+    public class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            this.Monitor.Log("SVFL is loaded!", LogLevel.Debug);
+        }
+    }
+}
